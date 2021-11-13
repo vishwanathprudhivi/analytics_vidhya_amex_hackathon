5914df5ea75f81d437d47343041c8aaf5c953298
vishwanathprudhivi <vishwanath.prudhivi@gmail.com>
2021-11-14 00:54:19 +0530

    s4 - change to nn arch

--- generate_model.py
+++ generate_model.py
@@ -7,13 +7,18 @@ from extra_keras_metrics import get_minimal_multiclass_metrics
 from sklearn.metrics import average_precision_score,accuracy_score
 from sklearn.multioutput import MultiOutputClassifier
 import xgboost as xgb
+import random
+from sklearn.datasets import make_classification
+from sklearn.neighbors import NearestNeighbors
 
 #import user defined libraries
 from constants import PROCESSED_TRAIN_PATH,PROCESSED_TEST_PATH,RAW_TEST_PATH,PREDICTION_FILE_PATH
 
+#set seed
+np.random.seed(2021)
+
 train_df  = pd.read_csv(PROCESSED_TRAIN_PATH)
-#'gender_Male', 'city_category_C2',
-       'customer_category_S2'
+
 feature_cols = ['age', 'vintage', 'is_active', , 'current_P16',
        'current_P13', 'current_P20', 'current_P11', 'current_P8',
        'current_P17', 'current_P21', 'current_P12', 'current_P10',
@@ -26,7 +31,135 @@ target_cols = ['future_P8', 'future_P3', 'future_P00', 'future_P6', 'future_P12'
        'future_P5', 'future_P7', 'future_P11', 'future_P2', 'future_P15',
        'future_P17', 'future_P14', 'future_P20', 'future_P18']
 
+#remove low event rates
+#target_cols = [col for col in target_cols if col not in ['future_P11','future_P2','future_P15','future_P17','future_P14','future_P20','future_P18']]
+
 x_train,x_val,y_train,y_val = train_test_split(train_df[feature_cols],train_df[target_cols],test_size = 0.2)
+
+def create_dataset(n_sample=1000):
+    ''' 
+    Create a unevenly distributed sample data set multilabel  
+    classification using make_classification function
+    
+    args
+    nsample: int, Number of sample to be created
+    
+    return
+    X: pandas.DataFrame, feature vector dataframe with 10 features 
+    y: pandas.DataFrame, target vector dataframe with 5 labels
+    '''
+    X, y = make_classification(n_classes=5, class_sep=2, 
+                           weights=[0.1,0.025, 0.205, 0.008, 0.9], n_informative=3, n_redundant=1, flip_y=0,
+                           n_features=10, n_clusters_per_class=1, n_samples=1000, random_state=10)
+    y = pd.get_dummies(y, prefix='class')
+    return pd.DataFrame(X), y
+
+def get_tail_label(df):
+    """
+    Give tail label colums of the given target dataframe
+    
+    args
+    df: pandas.DataFrame, target label df whose tail label has to identified
+    
+    return
+    tail_label: list, a list containing column name of all the tail label
+    """
+    columns = df.columns
+    n = len(columns)
+    irpl = np.zeros(n)
+    for column in range(n):
+        irpl[column] = df[columns[column]].value_counts()[1]
+    irpl = max(irpl)/irpl
+    mir = np.average(irpl)
+    tail_label = []
+    for i in range(n):
+        if irpl[i] > mir:
+            tail_label.append(columns[i])
+    return tail_label
+
+def get_index(df):
+  """
+  give the index of all tail_label rows
+  args
+  df: pandas.DataFrame, target label df from which index for tail label has to identified
+    
+  return
+  index: list, a list containing index number of all the tail label
+  """
+  tail_labels = get_tail_label(df)
+  index = set()
+  for tail_label in tail_labels:
+    sub_index = set(df[df[tail_label]==1].index)
+    index = index.union(sub_index)
+  return list(index)
+
+def get_minority_instace(X, y):
+    """
+    Give minority dataframe containing all the tail labels
+    
+    args
+    X: pandas.DataFrame, the feature vector dataframe
+    y: pandas.DataFrame, the target vector dataframe
+    
+    return
+    X_sub: pandas.DataFrame, the feature vector minority dataframe
+    y_sub: pandas.DataFrame, the target vector minority dataframe
+    """
+    index = get_index(y)
+    X_sub = X[X.index.isin(index)].reset_index(drop = True)
+    y_sub = y[y.index.isin(index)].reset_index(drop = True)
+    return X_sub, y_sub
+
+def nearest_neighbour(X):
+    """
+    Give index of 5 nearest neighbor of all the instance
+    
+    args
+    X: np.array, array whose nearest neighbor has to find
+    
+    return
+    indices: list of list, index of 5 NN of each element in X
+    """
+    if X.shape[0]>=5:
+        nbs=NearestNeighbors(n_neighbors=5,metric='euclidean',algorithm='kd_tree').fit(X)
+    else:
+        nbs=NearestNeighbors(n_neighbors=3,metric='euclidean',algorithm='kd_tree').fit(X)
+    euclidean,indices= nbs.kneighbors(X)
+    return indices
+
+def MLSMOTE(X,y, n_sample):
+    """
+    Give the augmented data using MLSMOTE algorithm
+    
+    args
+    X: pandas.DataFrame, input vector DataFrame
+    y: pandas.DataFrame, feature vector dataframe
+    n_sample: int, number of newly generated sample
+    
+    return
+    new_X: pandas.DataFrame, augmented feature vector data
+    target: pandas.DataFrame, augmented target vector data
+    """
+    indices2 = nearest_neighbour(X)
+    n = len(indices2)
+    new_X = np.zeros((n_sample, X.shape[1]))
+    target = np.zeros((n_sample, y.shape[1]))
+    for i in range(n_sample):
+        reference = random.randint(0,n-1)
+        neighbour = random.choice(indices2[reference,1:])
+        all_point = indices2[reference]
+        nn_df = y[y.index.isin(all_point)]
+        ser = nn_df.sum(axis = 0, skipna = True)
+        target[i] = np.array([1 if val>2 else 0 for val in ser])
+        ratio = random.random()
+        gap = X.loc[reference,:] - X.loc[neighbour,:]
+        new_X[i] = np.array(X.loc[reference,:] + ratio * gap)
+    new_X = pd.DataFrame(new_X, columns=X.columns)
+    target = pd.DataFrame(target, columns=y.columns)
+    new_X = pd.concat([X, new_X], axis=0)
+    target = pd.concat([y, target], axis=0)
+    return new_X, target
+
 
 def get_model_xgb(x_train,y_train):
     # create XGBoost instance with default hyper-parameters
@@ -40,10 +173,10 @@ def get_model_xgb(x_train,y_train):
 def get_model_dl(input_shape,output_shape,x_train,y_train):
     model = tf.keras.Sequential()
     model.add(layers.Input(input_shape))
-    model.add(layers.Dense(128,activation = tf.keras.layers.LeakyReLU(alpha=0.01)))
-    model.add(layers.Dense(128,activation = tf.keras.layers.LeakyReLU(alpha=0.01)))
-    model.add(layers.Dense(64,activation = tf.keras.layers.LeakyReLU(alpha=0.01)))
-    model.add(layers.Dense(32,activation = tf.keras.layers.LeakyReLU(alpha=0.01)))
+    model.add(layers.Dense(128,activation = 'relu'))
+    model.add(layers.Dense(128,activation = 'relu'))
+    model.add(layers.Dense(64,activation = 'relu'))
+    model.add(layers.Dense(32,activation = 'relu'))
     model.add(layers.Dense(output_shape,activation = 'sigmoid'))
     model.compile(optimizer = 'nadam',loss = 'binary_crossentropy', metrics = get_minimal_multiclass_metrics())
     history = model.fit(x_train, y_train,
@@ -52,8 +185,9 @@ def get_model_dl(input_shape,output_shape,x_train,y_train):
                         shuffle = True)
     return model
 
-#model = get_model_xgb(x_train,y_train)
-#accuracy_score(y_val,model.predict(x_val))
+#get minority class data
+x_sub, y_sub = get_minority_instace(x_train, y_train)           
+x_res,y_res = MLSMOTE(x_sub, y_sub, 500)
 
 dl_model = get_model_dl(len(feature_cols),len(target_cols),x_train,y_train)
 accuracy_score(y_val,dl_model.predict(x_val).round())
